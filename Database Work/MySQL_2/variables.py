eead52d20afb644012f4cf30b7a4af723fd58fad
Sebastian <bkutayo@gmail.com>
2022-11-24 19:53:33 +0300

    File creation and initial commit of MySQL_2

---
+++ Database Work/MySQL_2/variables.py
@@ -0,0 +1,52 @@
+i_colour =(
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "purple",
+    "pink",
+    "white",
+    "grey",
+    "black"
+)
+"""COLOUR list as tuple for catalogue data creation"""
+
+i_name =(
+    "toy_car",
+    "mouse",
+    "water_glass",
+    "litter_box",
+    "pet_bed",
+    "pen",
+    "table",
+    "hat",
+    "ring",
+    "boots",
+    "shot_glass",
+    "watch",
+    "underwear",
+    "shirt",
+    "pot",
+    "ROCK",
+    "pants",
+    "rug",
+    "light_bulb",
+    "notebook",
+    "case",
+    "ruber_duck"
+)
+"""ITEM list as tuple for catalogue data creation"""
+
+s_data = (
+    ("1", "Bmazon", "Istanbul", "Yildiz"),
+    ("2", "AtaExpress", "Izmir", "Buca"),
+    ("3", "Armut", "Istanbul", "Bahcelievler"),
+    ("4", "Samsun", "Samsun", "Atakum"),
+    ("5", "KacakCay", "Istanbul", "Yildiz"),
+    ("6", "KIRMISIZLAR", "Izmir", "Dikili")
+)
+
+
+
+
